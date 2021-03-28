b983fac74d86e515bb55520c008d73742f12d3f2
Baitemir <ferrari170604@gmail.com>
2021-03-28 21:26:12 +0600

    added random to darea

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let bottle = document.querySelector(".bottle");
 let nameInp = document.querySelector('#name');
 let btn = document.querySelector('.btn');
 let ind = 0;
-let i = 0
+
 const dareList = [
     "Съесть пачку мятных жвачек и запить минералкой",
     "Смотреть на одного кажодого из участников и если кто то засмеётся то они целуются",
@@ -14,7 +14,6 @@ const dareList = [
     "Понюхай других игроков и скажи, чем пахнет каждый",
     "Встань возле туалета и как швейцар открывай двери всем входящим",
 ]
-
 
 btn.addEventListener("click", () => {
     ind++;
@@ -28,14 +27,14 @@ btn.addEventListener("click", () => {
 
 bottle.addEventListener("click", () => {
     let randomNum = Math.floor(Math.random() * (1080 - 360)) + 360;
+    let randomNumb = Math.floor(Math.random() * ((dareList.length + 1) - 0)) + 0;
     bottle.style.transform = `rotate(${randomNum}deg)`;
     bottle.style.transition = 'transform 2s';
     dare.style.opacity = '100%';
     dare.style.zIndex = '3';
     dare.style.transitionDelay = '3s'
     dare.style.transitionDuration = '1s'
-    dare.innerText = dareList[i]
-    i++;
+    dare.innerText = dareList[randomNumb]
 
 })
 
